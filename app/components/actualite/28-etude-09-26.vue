d7c5bba48487d6df90f8d92bd5003ffc32cbcda0
Tim Yhrel Winkelmann <twinkelmann@protonmail.com>
2026-09-11 09:20:00 +0200

    Add actu

---
+++ app/components/actualite/28-etude-09-26.vue
@@ -0,0 +1,22 @@
+<template>
+  <app-actualite :actualite="actualite">
+    <p class="text-lg text-lime-700 text-center mb-4">
+      Vous rencontrez des difficultés à parler de sexualité avec votre
+      partenaire ?
+    </p>
+    <p class="text-lg">
+      Un accompagnement professionnel peut vous aider à ouvrir le dialogue, sans
+      jugement et dans le respect de chacun.
+    </p>
+  </app-actualite>
+</template>
+<script setup lang="ts">
+import type { Actualite } from '~/assets/content'
+
+const actualite: Actualite = {
+  date: 'septembre 2026',
+  image: '/images/actualites/28-etude-09-26.jpg',
+  lightImage: true,
+  id: 'actualite-28',
+}
+</script>
